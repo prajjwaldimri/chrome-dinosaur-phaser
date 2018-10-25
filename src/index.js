f83fc554dfe43e08862034b2b2812429ec5e8220
Prajjwal <prajjwaldimri@hotmail.com>
2018-10-25 11:46:26 +0530

    Added running, default and jump actions to player

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 /* global Phaser */
 import 'phaser';
 
-import { debug } from 'util';
 import SimpleScene from './scenes/simple-scene';
 
 const gameConfig = {
   width: 680,
-  height: 400,
+  height: 450,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+      debug: true,
+    },
+  },
+  backgroundColor: '#ffffff',
   scene: SimpleScene,
 };
 
-const game = new Phaser.Game(gameConfig);
-debug(game);
+new Phaser.Game(gameConfig); // eslint-disable-line
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,15 +1,78 @@
 /* global Phaser */
+
+let player;
+let ground;
+
 module.exports = class SimpleScene extends Phaser.Scene {
   preload() {
     this.load.image('cokecan', 'assets/cokecan.png');
-    this.load.spritesheet('player', 'assets/main-sprite.png', {
-      frameWidth: 32,
-      frameHeight: 48,
+    this.load.spritesheet('playerStanding', 'assets/player-stand.png', {
+      frameWidth: 88,
+      frameHeight: 103,
+    });
+    this.load.spritesheet('playerDucking', 'assets/player-duck.png', {
+      frameWidth: 118,
+      frameHeight: 100,
+    });
+    this.load.spritesheet('ground', 'assets/ground.png', {
+      frameWidth: 1202,
+      frameHeight: 24,
     });
   }
 
   create() {
-    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    this.physics.add.sprite(100, 450, 'player');
+    this.cursors = this.input.keyboard.createCursorKeys();
+    // Player Physics
+    player = this.physics.add.sprite(0, 300, 'player');
+    player.setSize(88, 90, false);
+    player.setCollideWorldBounds(true);
+
+    // Ground
+    ground = this.physics.add.staticGroup();
+    ground.create(0, 420, 'ground').refreshBody();
+
+    this.physics.add.collider(player, ground);
+
+    // PLayer Animations
+    this.anims.create({
+      key: 'running',
+      frames: this.anims.generateFrameNumbers('playerStanding', { start: 2, end: 3 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'ducking',
+      frames: this.anims.generateFrameNumbers('playerDucking', { start: 0, end: 1 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'default',
+      frames: [{ key: 'playerStanding', frame: 0 }],
+    });
+
+    this.anims.create({
+      key: 'dead',
+      frames: [{ key: 'playerStanding', frame: 4 }],
+    });
+
+    player.anims.play('running');
+    console.log(player.anims);
+  }
+
+  update() {
+    // Player Controls
+    if (this.cursors.up.isDown && player.body.touching.down) {
+      player.anims.play('default');
+      player.setVelocityY(-330);
+    } else if (this.cursors.down.isDown) {
+      player.anims.play('ducking');
+      player.setSize(118, 90, false);
+    } else if (player.anims.currentAnim.key !== 'running') {
+      // Play default running animation always
+      player.anims.play('running');
+    }
   }
 };
