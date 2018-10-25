f353978d215cfe6fd7c07be27398b93ef0abdf91
Prajjwal <prajjwaldimri@hotmail.com>
2018-10-25 10:17:03 +0530

    Added Eslint AirBNB Config

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,9 +1,15 @@
-export class SimpleScene extends Phaser.Scene {
+/* global Phaser */
+module.exports = class SimpleScene extends Phaser.Scene {
   preload() {
-    this.load.image("cokecan", "assets/cokecan.png");
+    this.load.image('cokecan', 'assets/cokecan.png');
+    this.load.spritesheet('player', 'assets/main-sprite.png', {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
   }
+
   create() {
-    this.add.text(100, 100, "Hello Phaser!", { fill: "#0f0" });
-    this.add.image(100, 200, "cokecan");
+    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+    this.physics.add.sprite(100, 450, 'player');
   }
-}
+};
